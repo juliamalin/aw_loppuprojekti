749ef93664338faf884ac4aeac2a647b45d73144
Niko <rappe.niko@gmail.com>
2023-03-31 12:06:02 +0300

    Projekti start

---
+++ server/src/main/java/net/projekti/serverapplication/ServerApplication.java
@@ -0,0 +1,15 @@
+package net.projekti.serverapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
