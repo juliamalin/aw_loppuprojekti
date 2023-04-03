18b68d1766bde993c57ccaa9afcd7dac1ac641d0
arska2 <aarni.seppanen@gmail.com>
2023-04-03 16:04:47 +0300

    google maps

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,24 @@ import { Provider } from "react-redux";
 import Navbar, { Header } from './main/headerfooter'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { TaskContainer } from './features/tasks/taskContainer';
-import {GoogleLogin} from './login/google';
+import { GoogleLogin } from './login/google';
+import { MapContainer } from './features/map/MapContainer';
+
 
 
 function App() {
+
   return (
     <Provider store={store}>
       <div className="App">
         <Navbar />
         <div className='taskit'>
           <p>Taskit</p>
-          <TaskContainer/>
-        </div>  
+          <TaskContainer />
+        </div>
         <div className='kartta'>
           <p>kartta</p>
+          <MapContainer />
         </div>
         <GoogleLogin />
       </div>
@@ -26,3 +30,5 @@ function App() {
 }
 
 export default App;
+
+
--- client/src/features/map/MapContainer.jsx
+++ client/src/features/map/MapContainer.jsx
@@ -1,40 +1,67 @@
-import React, { useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import React, { useCallback, useMemo, useRef } from "react";
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 
 
 
+const getUserLocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const userLocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            }
+            console.log("User at: ", userLocation)
+        })
+    } else {
+
+    }
+
+}
 
 
 
-
-export const MapContainer = ({ selectedTask, setSelectedTask }) => {
-
-    const [markers, setMarkers] = React.useState([])
-
+export const MapContainer = () => {
+    const { isLoaded, loadError } = useLoadScript({ googleMapsApiKey: "AIzaSyBWdAmavWXVzoZlEhuGBlyek4EfhS7i78A" })
+    const [markers, setMarkers] = React.useState([{ lat: 60.19, lng: 24.94 }])
+    const userLocation = getUserLocation()
     const options = {
-        disableDefaultUI: true,
+        disableDefaultUI: false,
         zoomControl: true
     }
 
-    const center = useMemo(() => ({ lat: 60.19, lng: 24.94 }))
+
+    const center = useMemo(() => userLocation || { lat: 60.19, lng: 24.94 })
 
     const addMarker = (event) => {
         const newMarker = {
             lat: event.latLng.lat(),
-            lng: event.latLng.lng()
+            lng: event.latLng.lng(),
         }
         setMarkers([...markers, newMarker])
     }
 
 
-    return (
+
+    if (!isLoaded) return <div>Loading...</div>
+    return (<div>
         <GoogleMap
             options={options}
             zoom={10}
             center={center}
-
+            mapContainerClassName="map-container"
+            onClick={addMarker}
         >
             {markers.map((marker, index) => (<Marker key={index} position={marker} />))}
-        </GoogleMap>)
+            {userLocation ? <Marker icon={{
+                fillColor: `#4285F4`,
+                fillOpacity: 1,
+                path: window.google.maps.SymbolPath.CIRCLE,
+                scale: 8,
+                strokeColor: `rgb(255,255,255)`,
+                strokeWeight: 2,
+            }} /> : ""}
+        </GoogleMap>
+
+    </div>)
 
 }
